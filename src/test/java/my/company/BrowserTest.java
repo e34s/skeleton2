bb2bc84dc11ab417bdb7d4a864ef6c17dcd4f56e
palotas <michael.palotas@element34.com>
2018-04-01 20:56:03 +0200

    commit message

---
+++ src/test/java/my/company/BrowserTest.java
@@ -0,0 +1,17 @@
+package my.company;
+
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BrowserTest extends TestBase{
+
+    @Test(description = "opening axa website")
+    public void simpleTest() {
+
+        EventFiringWebDriver driver = (EventFiringWebDriver) getDriver();
+        driver.get("https://www.axa.ch");
+        Assert.assertEquals(driver.getTitle(), "Private customers");
+        driver.quit();
+    }
+}
