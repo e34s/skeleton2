78d4b709d370d7f8d16a334730fad3b6e8ee50c5
palotas <michael.palotas@element34.com>
2018-04-03 16:17:00 +0200

    added Extent test without use of a listener

---
+++ src/test/java/simple/ExtentTestNoListener.java
@@ -0,0 +1,37 @@
+package simple;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import utils.MyListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ExtentTestNoListener {
+
+    protected static ExtentReports reports;
+    protected static ExtentTest test;
+
+    @Test
+    public void passedTest() {
+
+        reports = new ExtentReports(new SimpleDateFormat("yyyy-MM-dd hh-mm-ss-ms").format(new Date()) + "reports.html");
+
+        test = reports.startTest("passedTest()");
+        test.log(LogStatus.INFO, "test is started");
+        test.log(LogStatus.INFO, "doing something");
+
+
+        Assert.assertTrue(true);
+
+        reports.endTest(test);
+        reports.flush();
+
+    }
+
+}
