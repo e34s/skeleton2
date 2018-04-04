1a4bd26a1b61d820b9ee72eec03d49725f2ddb82
palotas <michael.palotas@element34.com>
2018-04-04 10:51:57 +0200

    renamed listener

--- src/test/java/utils/ExtentListener.java
+++ src/test/java/utils/ExtentListener.java
@@ -18,7 +18,7 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-    public class MyListener implements ITestListener {
+    public class ExtentListener implements ITestListener {
         protected static WebDriver driver;
         protected static ExtentReports reports;
         protected static ExtentTest test;
