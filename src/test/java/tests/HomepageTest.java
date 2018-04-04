b449e05043c7eec2d54d10a894ae5066f449c775
palotas <michael.palotas@element34.com>
2018-04-04 11:12:15 +0200

    added screnshot test - wip (not working yet but compiles)

--- src/test/java/tests/HomepageTest.java
+++ src/test/java/tests/HomepageTest.java
@@ -1,9 +1,13 @@
 package tests;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import utils.TestBase;
+
+import static utils.Helpers.screenshot;
 
 public class HomepageTest extends TestBase {
 
@@ -15,6 +19,8 @@ public class HomepageTest extends TestBase {
         HomePage hp = new HomePage(driver);
         hp.loadPage(driver);
         hp.clickCalculator();
+
+        screenshot(driver);
 
         Thread.sleep(5000);
     }
--- src/test/java/utils/Helpers.java
+++ src/test/java/utils/Helpers.java
@@ -1,17 +1,23 @@
 package utils;
 
 import org.openqa.selenium.OutputType;
-import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 
 public class Helpers {
 
-    public static final String SCREENSHOT_DIRECTORY = "C:\\Users\\mpalotas\\IdeaProjects\\axa-allure-testng-skeleton\\screenshots\\";
+    //public static final String SCREENSHOT_DIRECTORY = "C:\\Users\\mpalotas\\IdeaProjects\\axa-allure-testng-skeleton\\screenshots\\";
+    public static final String SCREENSHOT_DIRECTORY = "C:\\Users\\mpalotas\\Desktop\\screenshots\\";
 
 
-    public static void screenshot(EventFiringWebDriver driver) {
-        File tmp = driver.getScreenshotAs(OutputType.FILE);
+    public static void screenshot(WebDriver driver) {
+        WebDriver wd = new Augmenter().augment(driver);
+        File tmp = ((TakesScreenshot) wd).getScreenshotAs(OutputType.FILE);
+
         File ss = new File(SCREENSHOT_DIRECTORY + System.currentTimeMillis() + ".png");
         tmp.renameTo(ss);
         System.out.println("Screenshot: " + ss.getAbsoluteFile());
