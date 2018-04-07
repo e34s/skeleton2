8bca65342a2c99c6dd72f222a5ec70620a95afca
palotas <michael.palotas@element34.com>
2018-04-07 19:46:18 +0200

    completing allure tests

--- src/test/java/simple/AllureBrowserTest.java
+++ src/test/java/simple/AllureBrowserTest.java
@@ -1,6 +1,9 @@
 package simple;
 
 import io.qameta.allure.*;
+import javafx.application.Application;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
@@ -8,21 +11,58 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.TestBase;
 
+import java.io.File;
+
 import static utils.Helpers.screenshot;
 
 public class AllureBrowserTest extends TestBase {
 
-    @Test(description = "opening axa website")
+    @Test
     @Severity(SeverityLevel.CRITICAL)
-    @Feature("Browser Tests")
-    @Story("open AXA CH website")
+    @Feature("Feature 1")
+    @Story("Story 1")
     @Description("verify page title after opening AXA homepage")
-    public void simpleTest() {
+    public void openAxaWebsite() {
 
         WebDriver driver = getDriver();
         driver.get("https://www.axa.ch");
         Assert.assertEquals(driver.getTitle(), "Private customers");
         screenshot((RemoteWebDriver) driver);
+        saveScreenshot(((RemoteWebDriver) driver).getScreenshotAs(OutputType.BYTES));
         driver.quit();
+    }
+
+    @Test
+    @Severity(SeverityLevel.CRITICAL)
+    @Feature("Feature 2")
+    @Story("Story 1")
+    @Description("ZKB page can be openend")
+    public void openZKBWebsite() {
+
+        WebDriver driver = getDriver();
+        driver.get("https://www.zkb.ch");
+        Assert.assertTrue(true);
+        saveScreenshot(((RemoteWebDriver) driver).getScreenshotAs(OutputType.BYTES));
+        driver.quit();
+    }
+
+
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public byte[] saveScreenshot(byte[] screenShot) {
+        return screenShot;
+    }
+
+
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public byte[] saveScreenshot(RemoteWebDriver driver) {
+        System.out.println("calling allure screenshot");
+        return driver.getScreenshotAs(OutputType.BYTES);
+    }
+
+    public void screenshot(RemoteWebDriver driver) {
+        File tmp = driver.getScreenshotAs(OutputType.FILE);
+        File ss = new File("" + System.currentTimeMillis() + ".png");
+        tmp.renameTo(ss);
+        System.out.println("Screenshot: " + ss.getAbsoluteFile());
     }
 }
--- src/test/java/simple/ExtentTestWithListener.java
+++ src/test/java/simple/ExtentTestWithListener.java
@@ -1,16 +1,21 @@
 package simple;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import utils.ExtentListener;
+import utils.TestBase;
+
 
 @Listeners(ExtentListener.class)
-public class ExtentTestWithListener {
+public class ExtentTestWithListener extends TestBase {
 
     @Test
     public void passedTest() {
+
 
         Reporter.log("hello world");
         Assert.assertTrue(true);
@@ -19,6 +24,8 @@ public class ExtentTestWithListener {
 
     @Test
     public void failedTest() {
+
+        RemoteWebDriver driver = (RemoteWebDriver) getDriver();
 
         Reporter.log("hello world");
         Assert.assertTrue(false);
